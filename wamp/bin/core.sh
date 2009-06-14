7d9497f5bae3fda93edcb6c8d432abb9ae033aec
doug.muth <doug.muth@15c54c5c-d64c-0410-a204-274fbc44fa39>
2009-06-14 23:26:57 +0000

    Added WAMP-specific files.
    git-svn-id: https://anthrocon-reg.googlecode.com/svn/trunk@233 15c54c5c-d64c-0410-a204-274fbc44fa39

---
+++ wamp/bin/core.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+#
+# Our core file, which sets the location of WAMP, among other things.
+#
+
+WAMP="c:/wamp"
+PHP=${WAMP}/bin/php/php5.2.9-2/php.exe
+MYSQL=${WAMP}/bin/mysql/mysql5.1.33/bin/mysql.exe
+
+
