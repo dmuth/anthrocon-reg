fd7515e10716eb8aa3695749ca3d2a03537866c3
doug.muth <doug.muth@15c54c5c-d64c-0410-a204-274fbc44fa39>
2010-06-12 21:20:13 +0000

    Worked on some MySQL command line utilities.
    git-svn-id: https://anthrocon-reg.googlecode.com/svn/trunk@324 15c54c5c-d64c-0410-a204-274fbc44fa39

--- wamp/bin/core.sh
+++ wamp/bin/core.sh
@@ -6,6 +6,7 @@
 WAMP="c:/wamp"
 PHP=${WAMP}/bin/php/php5.3.0/php.exe
 MYSQL=${WAMP}/bin/mysql/mysql5.1.36/bin/mysql.exe
+MYSQLADMIN=${WAMP}/bin/mysql/mysql5.1.36/bin/mysqladmin.exe
 MYSQLDUMP=${WAMP}/bin/mysql/mysql5.1.36/bin/mysqldump.exe
 
 MYSQL_USER="root"
