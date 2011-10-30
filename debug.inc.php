23fdc757e95ffd94cd2593313b8ff11804f5b93c
Douglas Muth <dmuth@dmuth.org>
2011-10-30 14:44:50 -0400

    Wrote reg_debug_backtrace().

--- debug.inc.php
+++ debug.inc.php
@@ -21,3 +21,21 @@ function reg_debug($message) {
 
 } // End of reg_debug()
 
+
+/**
+* A wrapper for our reg_debug() function that includes a backtrace.
+*/
+function reg_debug_backtrace($message) {
+
+	if (is_array($message)) {
+		$message = print_r($message, true);
+	}
+
+	$trace = reg_log_get_backtrace(2);
+	$message .= "<br/><br/>Traceback:<br/>${trace}";
+
+	reg_debug($message);
+
+} // End of reg_debug_backtrace()
+
+
